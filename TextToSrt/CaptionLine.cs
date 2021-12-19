30ace68e48e0126823380ca5ec20f8c7caa214a9
angelikakuleta <kuleta.an@gmail.com>
2021-12-19 18:24:25 +0100

    Dump the paragraphs into the subtitles file

---
+++ TextToSrt/CaptionLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TextToSrt
+{
+    class CaptionLine
+    {
+        public string Content { get; }
+        public TimeSpan Duration { get; }
+
+        public CaptionLine(string content, TimeSpan duration)
+        {
+            Content = content.Trim();
+            Duration = duration;
+        }
+
+        public override string ToString() =>
+            $"{this.Duration} --> {this.Content}";
+    }
+}
---
+++ TextToSrt/TextDurationMeter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TextToSrt
+{
+    internal class TextDurationMeter
+    {
+        private double FullTextLength { get; }
+        private TimeSpan FullTextDuration { get; }
+
+        public TextDurationMeter(IEnumerable<string> fullText, TimeSpan clipDuration)
+        {
+            this.FullTextLength = fullText.Sum(this.CountRaedableLetters);
+            this.FullTextDuration = clipDuration;
+        }
+
+        public TimeSpan EstimateDuration(string text) =>
+            TimeSpan.FromMilliseconds(this.EstimateMilliseconds(text));
+
+        private double EstimateMilliseconds(string text) =>
+            this.FullTextDuration.TotalMilliseconds * this.GetRelativeLength(text);
+
+        private double GetRelativeLength(string text) =>
+            this.CountRaedableLetters(text) / this.FullTextLength;
+
+        private int CountRaedableLetters(string text) =>
+            Regex.Matches(text, @"w+").Sum(match => match.Value.Length);
+    }
+}
